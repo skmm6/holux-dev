eb2c95e68e989c84b5c5309293f677b57033cd0c
skmm6 <egor.qaq@gmail.com>
2022-09-27 17:16:17 +0300

    contact

--- src/app.js
+++ src/app.js
@@ -1,3 +1,47 @@
 import * as name from './js/modules/test';
+import * as accordions from './js/modules/accordion';
+// import Swiper JS
+  // core version + navigation, pagination modules:
+  import Swiper, { Navigation, Pagination } from 'swiper';
+  // import Swiper and modules styles
+  import 'swiper/css';
+  import 'swiper/css/navigation';
+  import 'swiper/css/pagination';
 
-name.testFunc();
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener('scroll', name.scrollHeader);
+accordions.accordions();
+
+//   // init Swiper:
+//   const swiper = new Swiper('.swiper', {
+//     // configure Swiper to use modules
+//     modules: [Navigation, Pagination],
+//   });
+
+/*============================SWIPER POPULAR ============*/
+
+const swiperPopularOb = new Swiper(".popular__container", {
+        spaceBetween: 32,
+        grabCursor: true,
+        centeredSlides: true,
+        sliderPerView: 'auto',
+        loop: true,
+
+        modules: [Navigation, Pagination],
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+});
+
--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,3 +1,13 @@
-export const testFunc = function(){
-    console.log('TeSt 2');
+/*=============== SHOW MENU ===============*/
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+export const scrollHeader = () =>{
+    const header = document.getElementById('header')
+
+    if(window.scrollY >= 50) {
+        header.classList.add('scroll-header')
+    } else {
+        header.classList.remove('scroll-header')
+    }
 }
+/*============================SWIPER POPULAR ============*/
